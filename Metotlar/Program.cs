bc06372f3590737c8a6468ab4025063290ab8151
Çağatay KOÇ <kocagatayy@gmail.com>
2021-02-24 21:17:37 +0300

    16 ocak ders sonu

---
+++ Metotlar/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya Elması";
+
+            Product product1 = new Product();
+            product1.Adi = "Elma";
+            product1.Fiyati = 15;
+            product1.Aciklama = "Amasya Elması";
+            product1.stokAdet = 5;
+
+
+            Product product2 = new Product();
+            product2.Adi = "Karpuz";
+            product2.Fiyati = 80;
+            product2.Aciklama = "Diyarbakır Karpuzu";
+            product2.stokAdet = 3;
+
+            Product[] products = new Product[] { product1,product2 };
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Adi);
+                Console.WriteLine(product.Aciklama);
+                Console.WriteLine(product.Fiyati);
+                Console.WriteLine(product.stokAdet);
+                Console.WriteLine("-----------------");
+            }
+
+            Console.WriteLine("-------------Metotlar---------------------");
+
+            //instace - class örneği oluşturmak - örnek
+            //encapsulation
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Add(product1);
+            sepetManager.Add(product2);
+
+
+            sepetManager.Add2(urunAdi = "armut", aciklama = "sarı", fiyati = 10, 5);
+            sepetManager.Add2(urunAdi = "ayva", aciklama = "sarı", fiyati = 10, 3);
+            sepetManager.Add2(urunAdi = "kiraz", aciklama = "kırmızı", fiyati = 10, 1);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {   // naming convention
+        //syntax
+        public void Add(Product product)
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi : " + product.Adi);
+
+        }
+        public void Add2(string urunAdi, string aciklama, double fiyat, int stokAdet)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urunAdi);
+
+        }
+
+
+    }
+}
